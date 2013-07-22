5e8e34740c4fb07ee19b6be610c99a6aa7b85658
Bova <vladimir@fido.uz>
2013-07-22 19:10:27 +0500

    quereing to SQ_Users works

--- palisade/ui/admin/views.py
+++ palisade/ui/admin/views.py
@@ -5,10 +5,15 @@ Created on Jul 16, 2013
 '''
 from flask import render_template, request
 from palisade.ui.admin import admin
+from palisade.db.schema import SQ_User
+from palisade.db.conn import Session
 
 @admin.route('/', defaults={'page': 'index'})
 @admin.route('/user')
 def show_users():
-    return render_template('show_users.html')
+    session = Session()
+    users = session.query(SQ_User).all()
+            
+    return render_template('show_users.html', users=users)
 
 
